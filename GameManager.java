4123c7729b14b10ac5a6d0c8aa6a173c44d53efa
rasmus <rasmus.b.bodker@gmail.com>
2023-09-25 16:21:58 +0200

    Added getActivePlayer method in GameManager

--- GameManager.java
+++ GameManager.java
@@ -22,4 +22,13 @@ public class GameManager {
     public void addPlayer(Player player) {
         this.players.add(player);
     }
+
+    /**
+     * Gets the active player, i.e the current player who's turn it is 
+     * The active player is the player in index 0 of the players list
+     * @return the active player
+     */
+    public Player getActivePlayer() {
+        return this.players.get(0);
+    }
 }
