a758bcd64d18a8d924d462da57ed35b79daf7e94
GusFromCPH <s235781@dtu.dk>
2023-09-25 16:14:01 +0200

    Scanner

---
+++ Player.java
@@ -0,0 +1,8 @@
+class Player {
+    public static void main(String[] args) {
+        var scanner = new java.util.Scanner(System.in);
+        scanner.useLocale(java.util.Locale.ENGLISH);
+
+        scanner.close();
+    }
+}
