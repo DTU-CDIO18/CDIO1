945f237a5c2d5e8713b2e75ca1a9110ba507f551
RoccoMM <s224961@student.dtu.dk>
2023-09-25 16:07:42 +0200

    - Created Main class - Added Scanner for user input - Added welcome message output

---
+++ Main.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main (String[] args){
+        var scanner = new java.util.Scanner(System.in);
+        System.out.println("Welcome to the Dicegame.");
+        
+    }
+}
