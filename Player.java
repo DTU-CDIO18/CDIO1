c32950ada6abc63c0251352f49a80be8a33620f3
GusFromCPH <s235781@dtu.dk>
2023-09-26 14:58:54 +0200

    Added variables

--- Player.java
+++ Player.java
@@ -1,5 +1,12 @@
+import java.util.UUID;
+
 class Player {
     public static void main(String[] args) {
+
+        String name = "";
+        UUID id = UUID.randomUUID();
+        int points = 0;
+
         var scanner = new java.util.Scanner(System.in);
         scanner.useLocale(java.util.Locale.ENGLISH);
 
