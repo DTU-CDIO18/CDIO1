07981c4d4c79264cfb3284c3e7d932cce82a4e6a
Daniel Lerche <daniel.lerche10@gmail.com>
2023-09-28 11:58:32 +0200

    Added roll function, including documentation, with missing functionality until the die class is made.

--- RaffleCup.java
+++ RaffleCup.java
@@ -7,4 +7,13 @@ class RaffleCup {
     RaffleCup(List<Die> dice) {
         this.dice = dice;
     }
+
+    /**
+     * Rolls all of the dice contained in the RaffleCup object
+     * and returns the result of the dice's
+     * @return an int[] of the dice results
+     */
+    public int[] roll() {
+        // TODO - Add functionality that rolls the all the dies and returns an int[] of the dice's result.
+    }
 }
