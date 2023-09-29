d9eb240532696a196824e17a80a3eb1c9fde8948
RoccoMM <s224961@student.dtu.dk>
2023-09-29 09:11:35 +0200

    Added player input and added the main game loop

--- Main.java
+++ Main.java
@@ -2,6 +2,29 @@ class Main {
     public static void main (String[] args){
         var scanner = new java.util.Scanner(System.in);
         System.out.println("Welcome to the Dicegame.");
+        System.out.println("Enter name of Player 1.");
+        var player1 scanner.nextLine();
+
+    //RQ 1. Find active player
+    
+    //RQ 2. Roll dice
+    
+    
+        System.out.println("Enter name of Player 2.");
+        var player2 scanner.nextLine();
+    //RQ "Main" flow loop.
+    
+    //RQ end game when points >=40 (or above).
+    while(player1points || player2points >= 40) {
+
+
+    }
+    
+    var player1points 
+    var player2points
+    System.out.printLN     
         
     }
+
+
 }
