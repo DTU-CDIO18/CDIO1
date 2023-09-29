ddc00aeccfdf022da6a0a0ae795f5c0d8179a6c1
rasmus <rasmus.b.bodker@gmail.com>
2023-09-29 09:41:29 +0200

    Reformat code, so it looks more prettier

--- Main.java
+++ Main.java
@@ -1,27 +1,24 @@
 import java.util.Scanner;
 
 class Main {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Dicegame.");
         System.out.println("Enter name of Player 1.");
         String player1 = scanner.nextLine();
 
-    //RQ 1. Find active player
-    
-    //RQ 2. Roll dice
-    
-    
+        // RQ 1. Find active player
+
+        // RQ 2. Roll dice
+
         System.out.println("Enter name of Player 2.");
         String player2 = scanner.nextLine();
-    //RQ "Main" flow loop.
-    
-    //RQ end game when points >=40 (or above).
-    while(player1points || player2points >= 40) {
+        // RQ "Main" flow loop.
 
+        // RQ end game when points >=40 (or above).
+        while (player1points || player2points >= 40) {
 
+        }
     }
-}
-
 
 }
