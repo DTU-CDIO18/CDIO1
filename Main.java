df05e2c26df5b1870c5ca322059e144b6339f16f
rasmus <rasmus.b.bodker@gmail.com>
2023-09-29 09:43:40 +0200

    Integrated use of the Player class

--- Main.java
+++ Main.java
@@ -5,14 +5,14 @@ class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Dicegame.");
         System.out.println("Enter name of Player 1.");
-        String player1 = scanner.nextLine();
+        Player player1 = new Player(scanner.nextLine());
 
         // RQ 1. Find active player
 
         // RQ 2. Roll dice
 
         System.out.println("Enter name of Player 2.");
-        String player2 = scanner.nextLine();
+        Player player2 = new Player(scanner.nextLine());
         // RQ "Main" flow loop.
 
         // RQ end game when points >=40 (or above).
